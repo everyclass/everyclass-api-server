7422b9469dd0911eba80b7ada9f02192df526389
wolfbolin <wolfbolin@foxmail.com>
2019-08-28 00:39:59 +0800

    Update http_response.php

--- server/src/util/http_response.php
+++ server/src/util/http_response.php
@@ -17,49 +17,49 @@ function Not_modified(Response $response)
     return $response->withStatus(304);
 }
 
-function Unauthorized(Response $response, $info = "需要验证身份")
+function Unauthorized(Response $response, $info = "Require authentication")
 {
     $error_info = ["status" => "error", "info" => $info];
     return $response->withStatus(401)->withJson($error_info);
 }
 
-function Unauthorized3(Response $response, $info = "身份验证失败")
+function Unauthorized3(Response $response, $info = "Authentication failure")
 {
     $error_info = ["status" => "error", "info" => $info];
     return $response->withStatus(401)->withJson($error_info);
 }
 
-function Bad_request(Response $response, $info = "访问参数异常")
+function Bad_request(Response $response, $info = "Access parameter exception")
 {
     $error_info = ["status" => "error", "info" => $info];
     return $response->withStatus(403)->withJson($error_info);
 }
 
-function Not_found(Response $response, $info = "无法找到页面文件")
+function Not_found(Response $response, $info = "Page file could not be found")
 {
     $error_info = ["status" => "error", "info" => $info];
     return $response->withStatus(404)->withJson($error_info);
 }
 
-function Not_allowed(Response $response, $info = "此请求方法被拒绝")
+function Not_allowed(Response $response, $info = "This request method was rejected")
 {
     $error_info = ["status" => "error", "info" => $info];
     return $response->withStatus(405)->withJson($error_info);
 }
 
-function Not_acceptable(Response $response, $info = "请求参数无法被响应")
+function Not_acceptable(Response $response, $info = "Request cannot be responsed")
 {
     $error_info = ["status" => "error", "info" => $info];
     return $response->withStatus(406)->withJson($error_info);
 }
 
-function Server_error(Response $response, $info = "服务器内部错误")
+function Server_error(Response $response, $info = "Server internal error")
 {
     $error_info = ["status" => "error", "info" => $info];
     return $response->withStatus(500)->withJson($error_info);
 }
 
-function Service_unavailable(Response $response, $info = "服务暂时不可用")
+function Service_unavailable(Response $response, $info = "Service temporarily unavailable")
 {
     $error_info = ["status" => "error", "info" => $info];
     return $response->withStatus(503)->withJson($error_info);
