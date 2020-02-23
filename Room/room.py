b9c4d05857710b20b3b6951f2e20977ac3f627ea
wolfbolin <wolfbolin@foxmail.com>
2020-02-24 01:28:17 +0800

    Room info api

--- Room/room.py
+++ Room/room.py
@@ -8,6 +8,30 @@ from Room.database import *
 from flask import current_app as app
 
 
+@Room.room_blue.route("/<code>")
+def room_info(code):
+    # 校验数据
+    if re.match(r"^\w+$", code) is None:
+        return Util.common_rsp("Unsupported code", status="Forbidden")
+
+    # 连接数据库
+    conn = app.mysql_pool.connection()
+
+    # 查询数据
+    room_base_info = read_room_info(conn, code)
+    available_semester = Common.read_available_semester(conn, code, "room")
+
+    # 格式调整
+    res = {
+        "status": "success"
+    }
+    res.update(room_base_info)
+    res["room_code"] = res.pop("code")
+    res["semester_list"] = available_semester
+
+    return jsonify(res)
+
+
 @Room.room_blue.route("/<code>/timetable/<semester>")
 def room_timetable(code, semester):
     # 校验数据
